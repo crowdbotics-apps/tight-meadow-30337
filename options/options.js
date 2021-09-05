0eeae0615a7e84e54e0b6ec02396626a46102013
Crowdbotics <team@crowdbotics.com>
2021-09-05 20:40:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tight_meadow_30337Identifier",
+  url: "https://tight_meadow_30337Identifier.botics.co",
+  api: "https://tight_meadow_30337Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
